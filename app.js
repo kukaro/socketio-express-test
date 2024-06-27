7ac6fde5191199e67a40966f1af22a65afa2553d
coeat.k <coeat.k@kakaocorp.com>
2024-06-27 14:55:31 +0900

    feat: 이미지 url 전송부분 추가

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true,
 }));
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    const query = req.query;
+    axios.get(`${query.url}`,{
+        responseType: 'arraybuffer',
+    })
+        .then((response) => {
+            res.setHeader('Content-Type', 'image/jpeg');
+            res.send(response.data);
+        }).catch((error) => {
+        res.send(error);
+    });
 });
 
 module.exports = router;
